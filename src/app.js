3b030dcccece17f53359bf3f4f5ca38e3dbb4354
Het Patel <hetp943@gmail.com>
2023-03-15 22:53:26 +0530

    added partials and handlebars

--- src/app.js
+++ src/app.js
@@ -2,27 +2,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const hbs = require('hbs');
+
 
 //public static path
 const staticPath = path.join(__dirname,"../public");
+const templatePath = path.join(__dirname,"../templates/views")
+const partialsPath = path.join(__dirname,"../templates/partials")
+
+
+app.set('view engine','hbs')
+app.set('views',templatePath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(staticPath));
-
 //routing
 app.get("/",(req,res)=>{
-    res.send("welcome to Het Patel's Website")
+    res.render('index')
 })
 
 app.get("/about",(req,res)=>{
-    res.send("welcome to Het Patel's about page")
+    res.render('about')
 })
 
 app.get("/weather",(req,res)=>{
-    res.send("welcome to Het Patel's weather page")
+    res.render("weather")
 })
 
 app.get("*",(req,res)=>{
-    res.send("404 error!!")
+    res.render("404error")
 })
 app.listen(port, () => {
     console.log(`listening to the port at ${port}`)
